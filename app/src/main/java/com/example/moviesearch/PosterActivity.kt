f82b26dfdcaee249e1c82df3ae4399299d7f263e
dabru <dabrusov@gmail.com>
2023-08-31 11:37:43 +0300

    1

---
+++ app/src/main/java/com/example/moviesearch/PosterActivity.kt
@@ -0,0 +1,32 @@
+package com.example.moviesearch
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bumptech.glide.Glide
+import com.example.moviesearch.ui.theme.MovieSearchTheme
+
+class PosterActivity : Activity() {
+
+    private lateinit var poster: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poster)
+        poster = findViewById(R.id.poster)
+        val url = intent.extras?.getString("poster", "")
+
+        Glide.with(applicationContext)
+            .load(url)
+            .into(poster)
+    }
+}
